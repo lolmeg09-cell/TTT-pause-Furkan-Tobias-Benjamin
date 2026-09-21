f26deab2e7f7c2c0cf858489978131453c7311a6
Furkan <lolmeg09@gmail.com>
2026-09-21 12:36:34 +0200

    fikk opp nettsiden

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express")
 const app = express();
 
-app.listen(4000);
+
 
 
 app.get("/", (req, res) => {
-  res.send("index")
+  res.render("index");
 });
 
-app.set("viewengine", "ejs") 
+app.set("view engine", "ejs") 
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.listen(3000 , () => {
+  console.log("http://localhost:3000/")
+});
+
+
