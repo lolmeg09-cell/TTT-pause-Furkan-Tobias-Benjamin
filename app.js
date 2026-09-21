39e80296973549267fac3e08ee919a9b06350ace
Blix <blixbenjamin@gmail.com>
2026-09-21 12:53:28 +0200

    kioi

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.get("/", (req, res) => {
 });
 
 app.set("viewengine", "ejs") 
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 
-app.listen(4000, () => {
-  console.log("http://localhost:4000");
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
 });
